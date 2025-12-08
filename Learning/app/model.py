93a22bd0e9abcc74f04b44bbd227c9f783a901d4
Shubham-Soni-Coder <slsitu1424@gmail.com>
2025-12-08 10:05:58 +0530

    Add doc-string in model.py file

--- Learning/app/model.py
+++ Learning/app/model.py
@@ -1,5 +1,7 @@
 from sqlmodel import SQLModel, Field
 
+"This is the model file where i create boiler palate of the database"
+
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
