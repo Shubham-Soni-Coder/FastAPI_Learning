e4464a249cd0e1b843376c6b23e8576a78faecc6
Shubham-Soni-Coder <slsitu1424@gmail.com>
2025-12-08 20:45:42 +0530

    all endpoints connect

--- project2/app/app.py
+++ project2/app/app.py
@@ -7,11 +7,16 @@ app = FastAPI()
 
 app.mount("/styles", StaticFiles(directory="styles"), name="styles")
 app.mount("/images", StaticFiles(directory="images"), name="images")
-
+app.mount("/templates", StaticFiles(directory="templates"), name="templates")
 # Templates
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
+@app.get("/", name="login")
 def show_form(request: Request):
     return templates.TemplateResponse("login_page.html", {"request": request})
+
+
+@app.get("/register", name="register")
+def show_register_form(request: Request):
+    return templates.TemplateResponse("register_page.html", {"request": request})
