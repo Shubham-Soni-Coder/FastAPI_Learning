b5ef2883ef417cb7607a7fd3e8e9ebb000c6373f
Shubham-Soni-Coder <slsitu1424@gmail.com>
2025-12-08 20:07:18 +0530

    connect uv and fastapi to html

--- project2/app/app.py
+++ project2/app/app.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/styles", StaticFiles(directory="styles"), name="styles")
+app.mount("/images", StaticFiles(directory="images"), name="images")
+
+# Templates
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def show_form():
-    with open("template/login_page.html", "r") as f:
-        return HTMLResponse(f.read())
+def show_form(request: Request):
+    return templates.TemplateResponse("login_page.html", {"request": request})
