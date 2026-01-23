2198bb2025d1604e912fef62228fafb62eef4f4e
Shubham-Soni-Coder <slsitu1424@gmail.com>
2026-01-23 07:32:21 +0530

    remove unwatched code from the app.function

--- project2/app/function.py
+++ project2/app/function.py
@@ -1,17 +1,2 @@
-import sqlite3
-
-
-def conn_database(query: str, parameter=None):
-    conn = sqlite3.connect("test.db")
-    cursor = conn.cursor()
-    if parameter:
-        cursor.execute(query, parameter)
-    else:
-        cursor.execute(query)
-    result = cursor.fetchall()
-    conn.close()
-    return result
-
-
 def normalize(name: str) -> str:
     return " ".join(name.strip().split())
